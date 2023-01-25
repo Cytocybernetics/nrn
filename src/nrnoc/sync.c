88380f673a7b9c10ee44770cb25364411b1cdd99
Lars Nilsson <nilsson@cytocybernetics.com>
2023-01-25 12:33:52 -0500

    Addinh synchronization code comments

--- src/nrnoc/sync.c
+++ src/nrnoc/sync.c
@@ -12,19 +12,71 @@
 #include <sys/ipc.h>
 #include "sync.h"
 
+/**
+ * @brief Shared memory file reference used with ftok()
+ * 
+ */
 static const char* shm_token = "/home/cytocybernetics/Cybercyte/DC1/SourceFiles/Shared_Memory/barriers";
+
+/**
+ * @brief Shared memory project id used with ftok()
+ * 
+ */
 static int shm_id = 1001;
 
 typedef struct
 {
+    /**
+     * @brief Number of threads or processes that will wait at the barrier.
+     * 
+     */
     unsigned int count;
+
+    /**
+     * @brief The number of threads or processes remaining to arrive at the barrier.
+     * 
+     */
     atomic_uint spaces;
+
+    /**
+     * @brief The current generation of waiting threads or processes.
+     * 
+     * A thread or process that is not last to arrive while spin-wait for its stored
+     * generation value at the time of starting to wait to be different from the
+     * current generation value of the barrier.
+     * 
+     * When the last thread or process to arrive at the barrier increments the
+     * generation value of the barrier, all waiting threads or processes notices this
+     * change and are then released from the barrier.
+     */
     atomic_uint generation;
+
+    /**
+     * @brief A flag indicating whether to abort the waiting at the barrier.
+     * 
+     */
     bool abort;
+
+    /**
+     * @brief The reason for aborting the wait at the barrier if it was performed programmatically by another thread or process
+     * 
+     */
     cyto_barrier_reason reason;
+
+    /**
+     * @brief A timeout value for the barrier
+     * 
+     * If the timeout value is 0 the barrier will prevent further execution indefinitely
+     * until all threads or processes has arrived at it.
+     * 
+     */
     long long timeout;
 } cyto_barrier_t;
 
+/**
+ * @brief A dynmically allocated array or barriers whose size is determined by the CytoBarrierSize enum value
+ * 
+ */
 static cyto_barrier_t* barriers;
 
 void cyto_barrier_init(cyto_barrier_t* barrier, int n)
@@ -46,6 +98,7 @@ void cyto_sync_init(bool creator)
         exit(1);
     }
 
+    // Allocate enough space for all barriers in the shared memory segment
     int shmid = shmget(key, CytoBarrierSize*sizeof(cyto_barrier_t), 0666 | IPC_CREAT);
 
     if (shmid == -1) {
@@ -53,6 +106,7 @@ void cyto_sync_init(bool creator)
         exit(1);
     }
 
+    // Get a pointer for the barrier array
     barriers = (cyto_barrier_t*)shmat(shmid, NULL, 0);
 
     if (barriers == NULL) {
@@ -60,8 +114,11 @@ void cyto_sync_init(bool creator)
         exit(1);
     }
 
-    if (creator) {
+    if (creator) { // Initialize all barriers if I am the creator
         for (size_t i = 0; i < CytoBarrierSize; ++i) {
+            // Initialize the barrier with an expected number of 2 threads or processes
+            // In the future we may want to make this more customizable on a per barrier
+            // basis, but for now it is a fixed number.
             cyto_barrier_init(&barriers[i], 2);
         }
     } else {
@@ -86,6 +143,11 @@ void cyto_barrier_set_timeout(size_t barrier_index, struct timespec timeout)
     barriers[barrier_index].timeout = timeout.tv_sec * 1000000000 + timeout.tv_nsec;
 }
 
+/**
+ * @brief Private function to reset barrier to a known usable state
+ * 
+ * @param barrier 
+ */
 static void cyto_barrier_reset(cyto_barrier_t* barrier)
 {
     barrier->spaces = barrier->count;
@@ -100,10 +162,10 @@ cyto_barrier_reason cyto_barrier_wait(cyto_barrier_id barrier_index)
 
     barrier->reason = CYTO_BARRIER_REASON_OK;
 
-    if (!--barrier->spaces) {
+    if (!--barrier->spaces) { // I was last to arrive at barrier, increment generation
         barrier->spaces = barrier->count;
         barrier->generation++;
-    } else {
+    } else { // I was not last to arrive at barrier, wait for generation increment (or timeout/break)
         struct timespec ts_start;
         struct timespec ts_current;
         long long start;
@@ -112,6 +174,8 @@ cyto_barrier_reason cyto_barrier_wait(cyto_barrier_id barrier_index)
         clock_gettime(CLOCK_MONOTONIC, &ts_start);
         start = ts_start.tv_sec * 1000000000 + ts_start.tv_nsec;
 
+        // Wait until the last thread or process to arrive increments the generation
+        // or a timeout or break condition occurs
         while (barrier->generation == my_generation) {
             if (barrier->abort) {
                 cyto_barrier_reset(barrier);
--- src/nrnoc/sync.h
+++ src/nrnoc/sync.h
@@ -8,6 +8,16 @@ extern "C" {
 #include <time.h>
 #include <stdbool.h>
 
+/**
+ * @brief Identifiers for the barriers.
+ * 
+ * The enum contains all valid identifiers for the barriers. Adding a new identifier will allocate another barrier,
+ * no other changes are required in the implementation to make it available.
+ * 
+ * It is important that CytoBarrierSize exists and is listed last, as it is used as the number of barriers, and is
+ * not an actual barrier id itself.
+ * 
+ */
 typedef enum {
     CytoBarrierStart,
     CytoBarrierLoopIndex,
@@ -24,10 +34,57 @@ typedef int cyto_barrier_reason;
 #define CYTO_BARRIER_REASON_BREAK 2
 #define CYTO_BARRIER_REASON_USER 1000
 
+/**
+ * @brief Initialize the synchronization barriers.
+ * 
+ * As the implementation uses shared memory, only one process must actually create
+ * the barriers, while the other simply maps it into memory.
+ * 
+ * Currently the barrier implementation expect there to be two threads or processes
+ * that will synchronize their execution. Changes to initialization code would be
+ * required in initialization to make this customizable for different barriers by
+ * specifying the required number of threads or processes for it.
+ * 
+ * @param creator Flag indicating whether tot create and initialize the barriers or not
+ */
 void cyto_sync_init(bool creator);
+
+/**
+ * @brief Free up barrier resources after they are no longer needed
+ * 
+ */
 void cyto_sync_cleanup();
+
+/**
+ * @brief Force barrier to break out of its wait state.
+ * 
+ * Calling this function will force the barrier to stop waiting and return the given reason (which can be some custom value).
+ * The place in the code where the barrier was waited upon can then make use of the reason to determine how to proceed if the
+ * value was not CYTO_BARRIER_REASON_OK.
+ * 
+ * @param barrier_index The barrier to break out of
+ * @param reason The reason for breaking out of the wait state
+ */
 void cyto_barrier_break(size_t barrier_index, cyto_barrier_reason reason);
+
+/**
+ * @brief Define the timeout value for the specified barrier.
+ * 
+ * The barrier index must be in the range 0 to CytoBarrierSize-1.
+ * 
+ * @param barrier_index The barrier index
+ * @param timeout The timeout value
+ */
 void cyto_barrier_set_timeout(size_t barrier_index, struct timespec timeout);
+
+/**
+ * @brief Wait for synchronization with another process using the specified barrier.
+ * 
+ * The barrier index must be in the range 0 to CytoBarrierSize-1.
+ * 
+ * @param barrier_id 
+ * @return cyto_barrier_reason 
+ */
 cyto_barrier_reason cyto_barrier_wait(cyto_barrier_id barrier_id);
 
 #ifdef __cplusplus
